94eb9824da4dbe8332e54b877e5b850c21ae45a2
arkaza369 <singharkaza271297@gmail.com>
2023-10-26 00:03:23 +0530

    add vehiclefactory

--- ParkingLotLLD/src/VehicleFactory.java
+++ ParkingLotLLD/src/VehicleFactory.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class VehicleFactory {
+import enums.VehicleType;
+
+public class VehicleFactory {
+    public Vehicle getVehicle(int vehicle_no, VehicleType vehicleType){
+        if(vehicleType.equals(VehicleType.TWOWHEELER)){
+            return new TwoWheeler(vehicle_no, vehicleType);
+        }
+        else if(vehicleType.equals(VehicleType.FOURWHEELER)){
+            return new FourWheeler(vehicle_no, vehicleType);
+        }
+        else if(vehicleType.equals(VehicleType.LARGEVEHICLE)){
+            return new LargeVehicle(vehicle_no, vehicleType);
+        }
+        else return null;
+    }
 }
